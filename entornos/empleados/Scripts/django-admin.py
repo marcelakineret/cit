00b73ee65bf27d805e41e4eb0cbf39c883ae873c
Kineret <kine.29@hotmail.com>
2020-05-11 16:47:28 -0500

    version 1

---
+++ entornos/empleados/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\kine2\documents\cursopro\entornos\empleados\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
